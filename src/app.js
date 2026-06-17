6f321525e9493652ef270a1b69acf483c32e01fe
Sandunika <amarasekarasandunika@gmail.com>
2026-06-17 21:39:50 +0530

    Implement auth setup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,44 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.route.js';
+import { timestamp } from 'drizzle-orm/gel-core';
+
 
 const app = express();
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions!');  
   res.status(200).send('Hello from acquisitions');
 });
+
+app.get('/health', (req,res)=>{
+    res.status(200).json({
+        status:'OK',
+        timestamp:new Date().toISOString(),
+        uptime: process.uptime()
+    })
+
+});
+
+app.get('/api', (req,res)=>{
+    res.status(200).json({
+        message:"Acquisition Api is Running"
+    })
+})
+
+app.use('/api/auth', authRoutes);
 
 export default app;
